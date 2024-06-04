9aef84e4dfcf47ea7657e0ecf4728273f07eca89
HellFireNeko <johntheojacob@gmail.com>
2024-06-05 00:01:31 +0200

    Initial VGA mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+#![no_std]
+#![no_main]
+
+mod io;
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
     println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    loop {}
 }
